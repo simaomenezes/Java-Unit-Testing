2a68af67be43f92f12c78d410e112702493a99a5
Simão Neto <simaomenezesneto@gmail.com>
2025-02-20 13:28:48 +0000

    [Section-3] feat: Make our first test and setting maven-surefire

---
+++ 0302/src/test/java/github/com/simaomenezes/FooBarTest.java
@@ -0,0 +1,14 @@
+package github.com.simaomenezes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class FooBarTest {
+
+    @Test
+    void Test(){
+        //fail("Not yet implemented");
+        System.out.println("Hello World");
+    }
+}

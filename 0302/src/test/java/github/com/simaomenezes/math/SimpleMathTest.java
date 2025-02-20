af20a030077e80db3ded3de938f141f9eb4e8cd4
Simão Neto <simaomenezesneto@gmail.com>
2025-02-20 17:30:55 +0000

    [Section-3] feat: Make other test

--- 0302/src/test/java/github/com/simaomenezes/math/SimpleMathTest.java
+++ 0302/src/test/java/github/com/simaomenezes/math/SimpleMathTest.java
@@ -17,4 +17,63 @@ public class SimpleMathTest {
         assertNotEquals(4d, result, ()->  "The testSun method should not return 4");
         assertNotNull(result, ()-> "The testSun method should not return null");
     }
+
+    @Test
+    void testDivision(){
+        SimpleMath simpleMath = new SimpleMath();
+
+        double firstNumber = 10d;
+        double secondNumber = 5d;
+        Double result = simpleMath.division(firstNumber, secondNumber);
+
+        double expected = 2d;
+        assertEquals(expected, result, () -> "The testDivision method should return: " + expected);
+    }
+
+    @Test
+    void testSubtraction(){
+        SimpleMath simpleMath = new SimpleMath();
+
+        double firstNumber = 10d;
+        double secondNumber = 5d;
+        Double result = simpleMath.subtraction(firstNumber, secondNumber);
+
+        double expected = 5d;
+        assertEquals(expected, result, () -> "The testSubtraction method should return: " + expected);
+    }
+
+    @Test
+    void testMultiplication(){
+        SimpleMath simpleMath = new SimpleMath();
+
+        double firstNumber = 10d;
+        double secondNumber = 5d;
+        Double result = simpleMath.multiplication(firstNumber, secondNumber);
+
+        double expected = 50d;
+        assertEquals(expected, result, () -> "The testMultiplication method should return: " + expected);
+    }
+
+    @Test
+    void testMean(){
+        SimpleMath simpleMath = new SimpleMath();
+
+        double firstNumber = 5d;
+        double secondNumber = 5d;
+        Double result = simpleMath.mean(firstNumber, secondNumber);
+
+        double expected = 5d;
+        assertEquals(expected, result, () -> "The testMean method should return: " + expected);
+    }
+
+    @Test
+    void testSquareRoot(){
+        SimpleMath simpleMath = new SimpleMath();
+
+        double anyNumber = 45d;
+        Double result = simpleMath.squareRoot(anyNumber);
+
+        double expected = 6.708203932499369d;
+        assertEquals(expected, result, () -> "The testSquareRoot method should return: " + expected);
+    }
 }

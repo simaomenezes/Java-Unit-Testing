64f12efd571d12e988bc8984b080d80db22e5569
Simão Neto <simaomenezesneto@gmail.com>
2025-02-20 17:20:56 +0000

    [Section-3] feat: Working with Assert messages, assert not equals and asserts not null, lazy assert messages

--- 0302/src/test/java/github/com/simaomenezes/math/SimpleMathTest.java
+++ 0302/src/test/java/github/com/simaomenezes/math/SimpleMathTest.java
@@ -2,15 +2,19 @@ package github.com.simaomenezes.math;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SimpleMathTest {
 
     @Test
     void testSun(){
         SimpleMath simpleMath = new SimpleMath();
-        Double result = simpleMath.sum(1d, 2d);
+        double firstNumber = 1d;
+        double secondNumber = 2d;
+        Double result = simpleMath.sum(firstNumber, secondNumber);
         Double expected = 3d;
-        assertEquals(expected, result);
+        assertEquals(expected, result, ()-> "The testSun method should return :" + expected);
+        assertNotEquals(4d, result, ()->  "The testSun method should not return 4");
+        assertNotNull(result, ()-> "The testSun method should not return null");
     }
 }

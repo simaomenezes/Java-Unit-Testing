b4cd594e7d1a74187aef5d1a204c4c060dfc7d46
Simão Neto <simaomenezesneto@gmail.com>
2025-03-12 16:21:49 +0000

    [Section-12] feat: Make test controller findAll and findById  Person - GET

--- 1003/src/test/java/github/com/simaomenezes/controllers/PersonControllerTest.java
+++ 1003/src/test/java/github/com/simaomenezes/controllers/PersonControllerTest.java
@@ -1,6 +1,8 @@
 package github.com.simaomenezes.controllers;
 
 import static org.hamcrest.CoreMatchers.is;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import github.com.simaomenezes.model.Person;
 import github.com.simaomenezes.services.PersonServices;
@@ -14,8 +16,11 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.BDDMockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -48,7 +53,7 @@ public class PersonControllerTest {
 
     @Test
     @DisplayName("Given Person Object when Create Person then Return Saved Person")
-    void testGivenPersonObject_whenCreatePerson_thenReturnSavedPerson() throws Exception {
+    void testGivenPersonObject_whenCreatePerson_thenReturnSavedPerson() throws JsonProcessingException, Exception {
         // Given / Arrange
         given(service.create(any(Person.class)))
                 .willAnswer((invocationOnMock -> invocationOnMock.getArgument(0)));
@@ -65,4 +70,53 @@ public class PersonControllerTest {
                 .andExpect(jsonPath("$.lastName", is(person.getLastName())))
                 .andExpect(jsonPath("$.email", is(person.getEmail())));
     }
+
+    @Test
+    @DisplayName("Given List of Persons when findAll Persons then Return Persons List")
+    void testGivenPersonsList_whenFindAllPersons_thenReturnPersonsList() throws Exception {
+        // Given / Arrange
+        List<Person> personList = new ArrayList<>();
+        personList.add(person);
+        personList.add(
+                new Person(
+                        "Raimundo",
+                        "Any",
+                        "fulano@gmail.com",
+                        "Address XXX",
+                        "Male"
+                )
+        );
+
+        given(service.findAll()).willReturn(personList);
+
+        // When / Act
+        ResultActions response = mockMvc.perform(get("/person"));
+
+        // Then / Assert
+
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.size()", is(personList.size())));
+
+    }
+
+    @Test
+    @DisplayName("Given personId when findById then return Person Object")
+    void testGivenPersonId_whenFindById_thenReturnPersonObject() throws Exception {
+        // Given / Arrange
+        Long personId = 1L;
+        given(service.findById(personId)).willReturn(person);
+
+        // When / Act
+        ResultActions response = mockMvc.perform(get("/person/{id}", personId));
+
+        // Then / Assert
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.firstName", is(person.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(person.getLastName())))
+                .andExpect(jsonPath("$.email", is(person.getEmail())));
+    }
+
+
 }

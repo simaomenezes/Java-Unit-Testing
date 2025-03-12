5d5f420d7206a0b70f55572623b2d8f0cd6ff8d1
Simão Neto <simaomenezesneto@gmail.com>
2025-03-12 16:26:44 +0000

    [Section-12] feat: Make test controller update person - PUT

--- 1003/src/test/java/github/com/simaomenezes/controllers/PersonControllerTest.java
+++ 1003/src/test/java/github/com/simaomenezes/controllers/PersonControllerTest.java
@@ -134,4 +134,35 @@ public class PersonControllerTest {
         response.andExpect(status().isNotFound())
                 .andDo(print());
     }
+
+    @Test
+    @DisplayName("Given Updated Person when Update then Return Updated Person Object")
+    void testGivenUpdatedPerson_WhenUpdate_thenReturnUpdatedPersonObject() throws JsonProcessingException, Exception {
+
+        // Given / Arrange
+        long personId = 1L;
+        given(service.findById(personId)).willReturn(person);
+        given(service.update(any(Person.class)))
+                .willAnswer((invocation) -> invocation.getArgument(0));
+
+        // When / Act
+        Person updatedPerson = new Person(
+                "Rombo",
+                "Silvestre",
+                "rambo@silvestre.com",
+                "Address XXX",
+                "Male");
+
+        ResultActions response = mockMvc.perform(put("/person")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(updatedPerson)));
+
+        // Then / Assert
+        response.
+                andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.firstName", is(updatedPerson.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(updatedPerson.getLastName())))
+                .andExpect(jsonPath("$.email", is(updatedPerson.getEmail())));
+    }
 }
